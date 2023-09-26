c318d286d7606f48750cf48e02fa4d46cc277acd
minnaingkoko <minnaingkoko2001@gmail.com>
2023-09-26 19:54:11 +0630

    fix deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,16 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+	},
+	build: {
+			rollupOptions: {
+				output:{
+					manualChunks(id) {
+						if (id.includes('node_modules')) {
+							return id.toString().split('node_modules/')[1].split('/')[0].toString();
+						}
+					}
+				}
+			}
+		}
+	});
